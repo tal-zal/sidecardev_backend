a61a24c12a8e186236b6b88ed521abb97fbf83b4
talzaloilov <tzaloilo@sfu.ca>
2023-05-20 23:36:05 -0700

    Changed cors allowance

--- backend.js
+++ backend.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 const firebaseRoutes = require('./firebaseRoutes');
-
+const cors = require('cors');
 
 // .../firebase/data -> gets sent to the firebaseRoutes file
 app.use('/firebase', firebaseRoutes);
+
+app.use(cors({
+    origin: '*'
+}));
 
 const port = 3000;
 app.listen(port, () => {
